b2c3da96c20b5dc1d753f5e6187c097d81ce35fc
qzhm2017 <qzhm1987@163.com>
2017-11-14 11:13:03 +0800

    新建蓝牙测试

---
+++ BleTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BleTool
+//
+//  Created by Mac on 2017/11/14.
+//  Copyright © 2017年 BeiJingXiaoMenTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BleTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BleTool
+//
+//  Created by Mac on 2017/11/14.
+//  Copyright © 2017年 BeiJingXiaoMenTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
